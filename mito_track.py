3772d503b54b96548f39fdbc8ee51fda0a87c391
marcpaga <marc.pages@live.com>
2020-01-30 17:42:12 +0100

    improved readibility on python script

--- mito_track.py
+++ mito_track.py
@@ -1,3 +1,4 @@
+# modules
 import cv2 as cv
 import numpy as np
 import tiffcapture as tc
@@ -6,6 +7,7 @@ import os
 import sys
 import copy
 
+## CONFIGURATION ==========
 # Parameters for Shi-Tomasi corner detection
 feature_params = dict(maxCorners = 1000, 
                       qualityLevel = 0.1, 
@@ -15,70 +17,90 @@ feature_params = dict(maxCorners = 1000,
 lk_params = dict(winSize = (15,15), 
                  maxLevel = 2, 
                  criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+                 
 # The video feed is read in as a VideoCapture object
-filename = 'dat/flow_wt_4.tif'
-name = filename.split('/')[1].split('.')[0]
-dots_dir = 'dots/' + name
-os.mkdir(dots_dir)
+filename = 'data/rawdata/flow_wt_1.tif'
+output_dir = 'data/processed/'
+visualize = True
+verbose = True
+## CONFIGURATION ==========
+
+
+name = filename.split('/')
+name = name[len(name)-1]
+name = name.split('.')[0]
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
+    
+output_dir_vid = output_dir + name + '/'
+if not os.path.isdir(output_dir_vid):
+    os.mkdir(output_dir_vid)
+
+   
 cap = tc.opentiff(filename)
+n_frames = cap.length
+
+## process the first frame
+j = 1
+if verbose:
+    print('Processing frame: ' + str(j) + '/' + str(n_frames), end='\r')    
+ 
 # Variable for color to draw optical flow track
 color = (0, 255, 0)
-# ret = a boolean return value from getting the frame, first_frame = the first frame in the entire video sequence
+# retrieve first frame
 ret, first_frame = cap.read()
 
-# Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
+# Converts frame to grayscale 
 prev_gray = first_frame
 
-prev_gray = prev_gray.astype(np.float64) / prev_gray.max() # normalize the data to 0 - 1
-prev_gray = 255 * prev_gray  # Now scale by 255
+# normalize the data to 0 - 1
+prev_gray = prev_gray.astype(np.float64) / prev_gray.max() 
+# Now scale by 255
+prev_gray = 255 * prev_gray  
 prev_gray = prev_gray.astype(np.uint8)
 
 
 prev = cv.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
-j = 1
-new_name = dots_dir + '/new_' + str(j) + '.csv'
-np.savetxt(new_name, prev[:,0,:], delimiter = ',')
 
-# Creates an image filled with zero intensities with the same dimensions as the frame - for later drawing purposes
+# write the point positions
+out_file = output_dir_vid + '/points_' + str(j) + '.csv'
+np.savetxt(out_file, prev[:,0,:], delimiter = ',')
+
+# Creates an image filled with zero intensities
 mask = np.zeros_like(first_frame)
 cap.release()
 
 cap = tc.opentiff(filename)
 
+## process all the rest of frames
 while(cap.isOpened()):
     j = j + 1
-    
-    # ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
-    ret, frame = cap.read()
-    # Converts each frame to grayscale - we previously only converted the first frame to grayscale
+    if verbose:
+        print('Processing frame: ' + str(j) + '/' + str(n_frames), end='\r')    
 
+    # get the frame
+    ret, frame = cap.read()
+    
+    # Converts each frame to grayscale 
     gray = frame
     gray = gray.astype(np.float64) / gray.max() # normalize the data to 0 - 1
     gray = 255 * gray  # Now scale by 255
     gray = gray.astype(np.uint8)
-    # Calculates sparse optical flow by Lucas-Kanade method
-    # https://docs.opencv.org/3.0-beta/modules/video/doc/motion_analysis_and_object_tracking.html#calcopticalflowpyrlk
+    
+    # calculate the flow
     nextt, status, error = cv.calcOpticalFlowPyrLK(prev_gray, gray, prev, None, **lk_params)
     
-    new_name = dots_dir + '/new_' + str(j) + '.csv'
+    # write the new point positions
+    out_file = output_dir_vid + '/points_' + str(j) + '.csv'
     out_tab = copy.copy(nextt[:,0,:])
     out_tab = np.append(out_tab, status, axis = 1)
     
-    np.savetxt(new_name, out_tab, delimiter = ',')
+    np.savetxt(out_file, out_tab, delimiter = ',')
     
     # Selects good feature points for previous position
     good_old = prev[status == 1]
     # Selects good feature points for next position
     good_new = nextt[status == 1]
-    
-    '''
-    if j == 4:
-        np.set_printoptions(threshold=sys.maxsize)
-        out_tab = copy.copy(nextt[:,0,:])
-        print(out_tab.shape)
-        print(np.append(out_tab, status, axis = 1))
-        exit()
-    '''
     
     # Draws the optical flow tracks
     for i, (new, old) in enumerate(zip(good_new, good_old)):
@@ -90,6 +112,7 @@ while(cap.isOpened()):
         mask = cv.line(mask, (a, b), (c, d), color, 2)
         # Draws filled circle (thickness of -1) at new position with green color and radius of 3
         frame = cv.circle(frame, (a, b), 3, color, -1)
+        
     # Overlays the optical flow tracks on the original frame
     output = cv.add(frame, mask)
     # Updates previous frame
@@ -98,13 +121,14 @@ while(cap.isOpened()):
     prev = good_new.reshape(-1, 1, 2)
     
     # Opens a new window and displays the output frame
-    # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
-    cv.imshow('frame',output)
-    k = cv.waitKey(30) & 0xff
-    time.sleep(0.1)
-    if k == 27:
+    # Frames are read by intervals of 10 milliseconds. 
+    if visualize:
+        cv.imshow('frame',output)
+        k = cv.waitKey(30) & 0xff
+        time.sleep(0.1)
+    
+    if j == n_frames:
         break
-    # Now update the previous frame and previous points
     
 # The following frees up resources and closes all windows
 cap.release()
